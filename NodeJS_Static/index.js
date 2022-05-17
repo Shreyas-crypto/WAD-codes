c1f3b9b2adfab6dfe88a764768cbb7a275e052ba
Shreyas Chaudhari <83209926+Shreyas-crypto@users.noreply.github.com>
2022-05-17 14:53:07 +0530

    Update index.js

--- NodeJS_Static/index.js
+++ NodeJS_Static/index.js
@@ -1,1 +1,10 @@
 
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+
+var server = app.listen(5000, () => {
+  console.log("Application running on port 5000")
+});
+\
